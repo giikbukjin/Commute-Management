71590b16fc1aa8ad0108eab25d81222768b9b776
Jinseo Jeong <js020315@gmail.com>
2024-05-21 09:26:27 +0900

    Spring Initializr

---
+++ src/main/java/com/inflearn/commute_management/CommuteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.commute_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuteManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuteManagementApplication.class, args);
+	}
+
+}
